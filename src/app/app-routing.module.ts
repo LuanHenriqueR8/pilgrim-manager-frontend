3c903fe0cdcdca674dcd439f04e88f52a74b4fcf
Luan Henrique Moraes dos Santos <luanhenriquer8@gmail.com>
2019-05-29 09:39:19 -0300

    pushing a clean project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './modules/home/home.module#HomeModule',
+    pathMatch: 'full'
+  },
+  { path: 'dashboard',
+    loadChildren: './modules/dashboard/dashboard.module#DashboardModule',
+  },
+  { path: 'kanban',
+    loadChildren: './modules/kanban/kanban.module#KanbanModule',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
